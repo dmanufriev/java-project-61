65167bc5e2dd8b51da007bf7dd5b70a8200f94a0
Dmitry Anufriev <dm.anufriev@gmail.com>
2023-11-07 11:37:23 +0300

    add greeting string to Cli.java

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,6 +3,7 @@ package hexlet.code;
 import java.util.Scanner;
 public class Cli {
     public static void greeting(Scanner scanner) {
+        System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
         String name = scanner.next();
         System.out.println("Hello, " + name + "!");
